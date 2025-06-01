90fe99e10dcc66bae4c52f979c4f6729f5aeeaea
webmaster254 <josephm2800@gmail.com>
2025-06-01 23:38:07 +0300

    feat: implement post creation modal with form and state management

--- alx-project-0x01/interfaces/index.ts
+++ alx-project-0x01/interfaces/index.ts
@@ -34,3 +34,15 @@ export interface PostProps {
   title: string;
   body: string;
 }
+
+export interface PostData {
+  userId: number;
+  id?: number;
+  title: string;
+  body: string;
+}
+
+export interface PostModalProps {
+  onClose: () => void;
+  onSubmit: (post: PostData) => void;
+}
