b49634ed57bfa83d6e3923b7fc9899e40e1c56fb
webmaster254 <josephm2800@gmail.com>
2025-06-01 23:33:58 +0300

    feat: add posts display and improve layout in users page

--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -1,15 +1,22 @@
 import Header from "@/components/layout/Header";
 import UserCard from "@/components/common/UserCard";
 import { UserProps } from "@/interfaces";
+import PostCard from "@/components/common/PostCard";
+import { PostProps } from "@/interfaces";
 
 const Users: React.FC<UserProps[]> = ({ users }) => {
     return (
-        <div>
+        <div className="flex flex-col h-screen">
             <Header />
-            <main>
+            <main className="p-4">
                 {
                     users?.map(({ id, name, username, email }: UserProps, key: number) => (
-                        <UserCard id={id} name={name} username={username} email={email} key={key} />
+                        <div key={key}>
+                        <UserCard id={id} name={name} username={username} email={email} />
+                        {posts.map(({ title, body, userId, id }: PostProps, key: number) => (
+                            <PostCard title={title} body={body} userId={userId} id={id} key={key} />
+                        ))}
+                        </div>
                     ))
                 }
             </main>
